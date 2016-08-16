8afd96c1a7c4980ee18fcd6b2d0fcd4e4a614765
surbhiposwalia <poswalia@ac87a31958e6.ant.amazon.com>
2016-08-16 10:21:26 -0700

    final updation

--- js_challenge1.js
+++ js_challenge1.js
@@ -3,13 +3,13 @@ function myfunction() {
     var i = 1;
     for (i = 1; i <= 30; i++) {
         if (((i % 3) == 0) && ((i % 5) == 0)) {
-            div.innerHTML = div.innerHTML + 'Fizz Buzz' + '<br />';
+            div.innerHTML += 'Fizz Buzz' + '<br />';
         } else if ((i % 3) == 0) {
-            div.innerHTML = div.innerHTML + 'Fizz' + '<br />';
+            div.innerHTML +=  'Fizz' + '<br />';
         } else if ((i % 5) == 0) {
-            div.innerHTML = div.innerHTML + 'Buzz' + '<br />';
+            div.innerHTML +=  'Buzz' + '<br />';
         } else {
-            div.innerHTML = div.innerHTML + i + '<br />';
+            div.innerHTML +=  i + '<br />';
         }
     }
 }
